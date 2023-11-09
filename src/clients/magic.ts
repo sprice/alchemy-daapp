7776c9c9c693eca988e397cbde278660647c3e18
Shawn Price <shawnp@gmail.com>
2023-11-09 12:22:37 -0800

    make it so

---
+++ src/clients/magic.ts
@@ -0,0 +1,26 @@
+import { WalletClientSigner } from "@alchemy/aa-core";
+import { Magic } from "magic-sdk";
+import { createWalletClient, custom } from "viem";
+import { env } from "~/env.mjs";
+
+// this is generated from the [Magic Dashboard](https://dashboard.magic.link/)
+const MAGIC_API_KEY = env.NEXT_PUBLIC_MAGIC_API_KEY;
+
+// instantiate Magic SDK instance
+export const magic = new Magic(MAGIC_API_KEY);
+
+// NOTE: because this is async, you will need to put this in a useEffect hook if using React
+export const createMagicSigner = async () => {
+  // 1. Authenticate the user (for other methods see magic docs https://magic.link/docs/dedicated/overview)
+  await magic.wallet.connectWithUI();
+
+  // 2. create a wallet client
+  const magicClient = createWalletClient({
+    transport: custom(await magic.wallet.getProvider()),
+  });
+
+  return new WalletClientSigner(
+    magicClient,
+    "magic" // signerType
+  );
+};
